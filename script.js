113dac2db8e27a6f26903af3bc900c96023f717b
Patrick Rocha <patrick.rochapereira@hotmail.com>
2023-11-03 15:30:11 -0300

    commit

--- script.js
+++ script.js
@@ -28,14 +28,18 @@ document.querySelector('#btnSignup')
 
 function iniciaModal (modalID) {
     const modal =  document.getElementById(modalID);
-    if(modal) {
+    
+    if(modal) {        
         modal.classList.add('mostrar');
+        
         modal.addEventListener('click', (e) => {
             if(e.target.id == modalID || e.target.className == 'fechar'){
                 modal.classList.remove('mostrar');
             }     
+            
         });
     }
+    
 }
 
 const logo = document.querySelector('#logar')
